8a4b643b4033dc5b6d0d98c7e23fc6c62f0bc3a0
MiliAnand <82170364+MiliAnand@users.noreply.github.com>
2021-06-18 14:58:04 +0530

    Add files via upload

---
+++ design/src/rvmyth_avsddac.v
@@ -0,0 +1,19 @@
+`include "mythcore_test.v"
+//`include "avsddac.v"
+module rvmyth_avsddac( clk, reset, Out);
+input clk, reset;
+output Out;
+
+wire Out;
+wire [9:0] rvtodac;
+wire VSS, VDD, VREFL, VREFH;
+
+assign VREFH = 3.3;
+assign VREFL = 0.0;
+assign VDD = 1.8;
+assign VSSA = 0.0;
+
+core uut1 ( .clk(clk), .reset(reset), .out(rvtodac));
+avsddac uut2 ( .OUT(Out), .D(rvtodac), .VREFH(VREFH), .VREFL(VREFL) );
+
+endmodule
